dfc85ecfc74ddcf62d9ed43cec18060ce3701b6b
Michael Muehlbeyer <mmuehlbeyer@confluent.io>
2023-11-16 21:57:45 +0100

    adding postgres scripts

---
+++ postgres/ddl/preps.sql
@@ -0,0 +1,10 @@
+--disclaimer
+-- not intended for production usage !
+
+-- set proper user rights for postgres user
+ALTER USER postgres WITH REPLICATION;
+
+-- create tables and functions
+-- use artist.sql and customer
+
+
